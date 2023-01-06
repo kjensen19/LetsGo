e98e79811d612cb34148180f51e39c4eef7eecc4
Kyle Jensen <kjensen19@gmail.com>
2023-01-05 22:16:26 -0600

    added in path for greetings module

--- hello.go
+++ hello.go
@@ -2,17 +2,22 @@ package main
 
 // Delcares a package "main": package is a way to group functions, made up of all files in same dir
 
-import "fmt"
+import (
+	"fmt"
 
-// Part of standard library, for formatting text
+	"example.com/greetings"
 
-import "rsc.io/quote"
+	// Part of standard library, for formatting text
+
+	"rsc.io/quote"
+)
 
 //To add as a requirement and generate a go.sum for authenticating:
 //$ go mod tidy
 
 func main() {
-	fmt.Println(quote.Go())
+	message := greetings.Hello("Kyle")
+	fmt.Println(message)
 }
 
 //main function executes by default when main package runs ($go run .)
